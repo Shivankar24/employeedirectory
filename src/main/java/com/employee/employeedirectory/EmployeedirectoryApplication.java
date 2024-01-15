9c5ff1522fefe7706aa2c07f02d9464ccee7a2bb
Shivankar Ojha <shivankar.ojha@H9JDPXYF6C.local>
2024-01-15 18:56:52 -0500

    reafactor packages

--- src/main/java/com/employee/employeedirectory/EmployeedirectoryApplication.java
+++ src/main/java/com/employee/employeedirectory/EmployeedirectoryApplication.java
@@ -1,13 +1,22 @@
 package com.employee.employeedirectory;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-public class EmployeedirectoryApplication {
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan(basePackages="com.employee.employeedirectory")
+@EnableJpaRepositories("com.employee.employeedirectory.Repository")
+public class EmployeeDirectoryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeedirectoryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeDirectoryApplication.class, args);
+    }
 
 }
